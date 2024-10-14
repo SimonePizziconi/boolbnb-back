c570845c2ee57f1eb08c73d9875e283283841fe3
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-14 16:59:56 +0200

    feat: add user table & validation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -29,16 +29,43 @@ class RegisteredUserController extends Controller
      * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request): RedirectResponse
+
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        // Calcola la data di 18 anni fa
+        $date18YearsAgo = now()->subYears(18)->toDateString();
+
+        $request->validate(
+            [
+                'first_name' => ['string','min:2', 'max:255'],
+                'last_name' => ['string','min:2','max:255'],
+                'birth_date' => ['date', 'before_or_equal:'.$date18YearsAgo],
+                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            ],
+            [
+                'first_name.string' => 'Il Nome non deve contenere numeri o caratteri speciali',
+                'first_name.min' => 'Il nome deve contenere almeno :min caratteri.',
+                'first_name.max' => 'Il nome non può superare i :max caratteri.',
+                'last_name.string' => 'Il Cognome non deve contenere numeri o caratteri speciali',
+                'last_name.min' => 'Il cognome deve contenere almeno :min caratteri.',
+                'last_name.max' => 'Il cognome non può superare i :max caratteri.',
+                'birth_date.date' => 'La data di nascita non è valida.',
+                'birth_date.before_or_equal' => 'Devi essere maggiorenne per registrarti!',
+                'email.required' => 'L\'email è obbligatoria.',
+                'email.lowercase' => 'L\'email deve essere in minuscolo.',
+                'email.email' => 'L\'email non è valida.',
+                'email.max' => 'L\'email non può superare i :max caratteri.',
+                'email.unique' => 'Questa email è già in uso.',
+                'password.required' => 'La password è obbligatoria.',
+                'password.confirmed' => 'Le password non corrispondono.',
+            ]
+        );
 
         $user = User::create([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
+            'birth_date' => $request->birth_date,
             'password' => Hash::make($request->password),
         ]);
 
