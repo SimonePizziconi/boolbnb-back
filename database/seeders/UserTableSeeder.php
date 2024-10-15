9846013ae820050e87c56d213160c190b2a24970
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-15 11:21:31 +0200

    feat: add user test seeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserTableSeeder extends Seeder
 {
@@ -12,6 +14,23 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = [
+            [
+                'first_name' => 'Admin',
+                'last_name' => 'Test',
+                'email' => 'admin@test.it',
+                'password' => Hash::make('321321321')
+            ],
+            [
+                'first_name' => 'Admina',
+                'last_name' => 'Test',
+                'email' => 'admina@test.it',
+                'password' => Hash::make('321321321')
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
