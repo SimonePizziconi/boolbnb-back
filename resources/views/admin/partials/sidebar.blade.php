c314db2040ef3d8b5c611f05f14d821aa99172dc
ValerioViteritti <vviteritti@gmail.com>
2024-10-15 17:51:33 +0200

    fix: generals.scss, sidebar.blade.php

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 text-white my_sidebar">
     <a id="logo_debug" href="/" class="align-items-center mb-3 mb-md-0 me-md-auto d-flex">
-        <img class="img-fluid w-25" src="{{ asset('img/logo-bool-bnb.png') }}" alt="logo">
+        <img class="img-fluid w-28" src="{{ asset('img/logo-bool-bnb.png') }}" alt="logo">
     </a>
 
     {{-- <div id="logo_debug"></div> --}}
@@ -8,22 +8,26 @@
     <ul class="nav nav-pills flex-column mb-auto">
         <li>
             <a href="{{ route('admin.index') }}" class="nav-link text-white">
-                <i class="fa-solid fa-house"> Home</i>
+                <span><i class="fa-solid fa-house"></i> Home</span>
+
             </a>
         </li>
         <li>
             <a href="{{ route('admin.apartments.index') }}" class="nav-link text-white d-flex">
-                <i class="fa-solid fa-list"> Appartamenti</i>
+                <span><i class="fa-solid fa-list"></i> Appartamenti</span>
+
             </a>
         </li>
         <li>
             <a href="{{ route('admin.apartments.create') }}" class="nav-link text-white">
-                <i class="fa-solid fa-plus"> Aggiungi Appartamento</i>
+                <span><i class="fa-solid fa-plus"></i> Aggiungi Appartamento</span>
+
             </a>
         </li>
         <li>
             <a href="{{ route('admin.apartments.trash') }}" class="nav-link text-white">
-                <i class="fa-solid fa-trash"> Cestino</i>
+                <span><i class="fa-solid fa-trash"></i> Cestino</span>
+
             </a>
         </li>
     </ul>
