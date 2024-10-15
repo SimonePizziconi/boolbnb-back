860c4e756b66b5cfd4440ad0b9c126f819e0ccd3
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-15 15:29:23 +0200

    feat: add delete apartments crud

--- resources/views/admin/apartments/trash.blade.php
+++ resources/views/admin/apartments/trash.blade.php
@@ -1,10 +1,40 @@
 @extends('layouts.app')
 
-
 @section('content')
+    <div class="container my-4">
+        <h1 class="my-5">Cestino</h1>
+        @if (!$apartments->isEmpty())
+            <ul class="list-group m-4">
 
+                @foreach ($apartments as $apartment)
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <span><strong>Id: </strong> {{ $apartment->id }} || <strong>Titolo:</strong>  {{ $apartment->title }} </span>
+                        <div>
+                            <form class="d-inline" action="{{route('admin.projects.restore', $apartment)}}" method="POST">
+                                @csrf
+                                @method('PATCH')
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fa-solid fa-trash-can-arrow-up"></i>
+                                </button>
+                            </form>
+                            <form class="d-inline" action="{{route('admin.projects.delete', $apartment)}}" method="POST" onsubmit="return confirm('Sei sicuro di voler eliminare definitivamente questo appartamento?')">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">
+                                    <i class="fa-solid fa-trash-can"></i>
+                                </button>
+                            </form>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <div class="alert alert-success text-center">
+                Il Cestino è vuoto!
+            </div>
+        @endif
+    </div>
 @endsection
-
 
 @section('title')
     Trash
