7997f8b3694f4abd5a1175b4fb9f1b660d260aaf
ValerioViteritti <vviteritti@gmail.com>
2024-10-15 15:46:52 +0200

    feat: add eye to login password

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,17 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+document.getElementById('togglePassword').addEventListener('click', function (e) {
+    // Ottieni l'input della password
+    const password = document.getElementById('password');
+
+    // Ottieni il tipo di input attuale e alterna tra 'password' e 'text'
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    // Cambia l'icona dell'occhio
+    this.querySelector('i').classList.toggle('fa-eye');
+    this.querySelector('i').classList.toggle('fa-eye-slash');
+});
+
