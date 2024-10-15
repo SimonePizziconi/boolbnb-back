f9b43a02f54411cdd058ae953de19666cdfac583
ValerioViteritti <vviteritti@gmail.com>
2024-10-15 10:55:17 +0200

    minor fix

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,5 +1,6 @@
-<div class="d-flex flex-column flex-shrink-0 p-3 text-white my_side" >
-    <a id="logo_debug" href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+<div class="d-flex flex-column flex-shrink-0 p-3 text-white my_side">
+    <a id="logo_debug" href="/"
+        class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
         <img src="" alt="logo">
     </a>
     {{-- <div id="logo_debug"></div> --}}
@@ -11,8 +12,8 @@
             </a>
         </li>
         <li>
-            <a href="#" class="nav-link text-white">
-                Link
+            <a href="http://127.0.0.1:8000/admin/apartments" class="nav-link text-white">
+                Appartamenti
             </a>
         </li>
         <li>
