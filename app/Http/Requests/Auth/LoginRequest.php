f31b4766c453902cfe73b5864331f26a01e31b63
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-16 10:09:34 +0200

    fix: login error message

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -57,7 +57,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'email' => __('Le credenziali inserite non sono corrette.'),
             ]);
         }
 
