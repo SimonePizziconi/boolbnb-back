35f30dbc9ac978a47ae6f148c8103bcced054a66
BeneArp <benedetta.arpino@gmail.com>
2024-10-16 10:41:15 +0200

    feat: add services table seeder

--- database/seeders/ServiceTableSeeder.php
+++ database/seeders/ServiceTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Service;
+use App\Functions\Helper;
 
 class ServiceTableSeeder extends Seeder
 {
@@ -12,6 +14,54 @@ class ServiceTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $the_services = [
+            'WiFi',
+            'Posto Macchina',
+            'Piscina',
+            'Portineria',
+            'Sauna',
+            'Vista Mare',
+            'Aria Condizionata',
+            'Riscaldamento',
+            'TV via Cavo',
+            'Cucina Completa',
+            'Lavastoviglie',
+            'Lavatrice',
+            'Asciugatrice',
+            'Balcone',
+            'Giardino Privato',
+            'Barbecue',
+            'Vasca Idromassaggio',
+            'Camino',
+            'Accesso Disabili',
+            'Animali Ammessi',
+            'Palestra',
+            'Spa',
+            'Servizio in Camera',
+            'Colazione Inclusa',
+            'Ristorante in Loco',
+            'Navetta Aeroporto',
+            'Parco Giochi',
+            'Deposito Bagagli',
+            'Parcheggio Bici',
+            'Noleggio Auto',
+            'Servizio di Pulizia',
+            'Reception 24 Ore',
+            'Cassaforte',
+            'Frigorifero',
+            'Mini Bar',
+            'Servizio Lavanderia',
+            'Cambio Biancheria',
+            'Vista Montagna',
+            'Vicino alla Spiaggia',
+            'Accesso alla Pista Ciclabile'
+        ];
+
+        foreach($the_services as $service){
+            $new_service = new Service;
+            $new_service->name = $service;
+            $new_service->slug = Helper::generateSlug($new_service->name, Service::class);
+            $new_service->save();
+        }
     }
 }
