8c0952b2e29fb16528cda3c9f6250494e51ab7d5
BeneArp <benedetta.arpino@gmail.com>
2024-10-22 15:32:27 +0200

    feat: add new message to messages table

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,7 @@
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 
