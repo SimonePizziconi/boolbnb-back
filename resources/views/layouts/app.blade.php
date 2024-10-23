5fa8dab2d611cf0b5e3495430e324323474a4550
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-23 12:47:49 +0200

    fix: bootstrap cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,8 +34,7 @@
     <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js">
     </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 
