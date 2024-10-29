576d5f9850dac0d828de1b03da82d0376cd437b8
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-29 13:06:20 +0100

    feat: add visualization charts

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Apartment;
+use App\Models\Statistic;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 
 
 class DashboardController extends Controller
@@ -15,8 +17,27 @@ class DashboardController extends Controller
     {
         $apartmentCount = Apartment::where('user_id', Auth::user()->id)->count();
 
+        // Recupera gli appartamenti con le statistiche
+        $apartments = Apartment::where('user_id', Auth::user()->id)->with('statistics')->get();
+
+        // Organizza i dati per visualizzazioni mensili per ciascun appartamento
+        $data = $apartments->map(function ($apartment) {
+            $monthlyViews = $apartment->statistics->groupBy(function ($statistic) {
+                // Raggruppa per anno e mese
+                return Carbon::parse($statistic->created_at)->format('Y-m');
+            })->map(function ($statsPerMonth) {
+                // Conta le visualizzazioni per ogni mese
+                return $statsPerMonth->count();
+            });
+
+            return [
+                'title' => $apartment->title,
+                'monthly_views' => $monthlyViews
+            ];
+        });
+
         $userName = Auth::user()->first_name;
 
-        return view('admin.index', compact('apartmentCount', 'userName'));
+        return view('admin.index', compact('apartmentCount', 'userName', 'data'));
     }
 }
