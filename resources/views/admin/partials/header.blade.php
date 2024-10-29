2e7331385e0db6f9701824a9588f05a0f180b1e3
SimonePizziconi <simonepizziconi05@gmail.com>
2024-10-29 18:38:32 +0100

    fix: header login

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,127 +1,92 @@
 <header>
-    {{-- <nav class="">
-        <div class="container">
-
-            <a class="navbar-brand">
-                <h2 class="my_txt_primary"></h2>
-            </a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropstart">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                <i class="fa-solid fa-user my_icon d-none d-md-inline-block d-lg-inline-block"></i>
-                                <span
-                                    class="d-block mt-1">{{ Auth::user()->first_name . ' ' . Auth::user()->last_name }}</span>
-                            </a>
-
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('admin.index') }}">{{ __('Dashboard') }}</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav> --}}
     <nav class="navbar navbar-expand-md d-flex align-items-center navbar-light shadow-sm">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ route('admin.index') }}">Dashboard</a>
+
+            <!-- Bottone per il menu offcanvas -->
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
                 aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
+            <!-- Menu offcanvas -->
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
                 aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
                     <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
+
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item d-inline-block d-md-none d-lg-none">
-                            <a id="logo_debug" href="http://localhost:5174/"
-                                class="align-items-center mb-3 mb-md-0 me-md-auto d-flex w-25">
-                                <img class="img-fluid" src="{{ asset('img/small-logo.png') }}" alt="logo">
-                            </a>
-                        </li>
-                        <li class="nav-item d-inline-block d-md-none d-lg-none">
-                            <a class="nav-link" aria-current="page"
-                                href="{{ route('admin.apartments.index') }}"><span><i class="fa-solid fa-list"></i>
-                                    Appartamenti</span></a>
-                        </li>
-                        <li class="nav-item d-inline-block d-md-none d-lg-none">
-                            <a class="nav-link" aria-current="page"
-                                href="{{ route('admin.apartments.create') }}"><span><i class="fa-solid fa-plus"></i>
-                                    Nuovo Appartamento</span></a>
-                        </li>
-                        <li class="nav-item d-inline-block d-md-none d-lg-none">
-                            <a class="nav-link" aria-current="page"
-                                href="{{ route('admin.sponsorships.payment') }}"><span><i class="fa-solid fa-star"></i>
-                                    Sponsorizza appartamento</span></a>
-                        </li>
-                        <li class="nav-item d-inline-block d-md-none d-lg-none">
-                            <a class="nav-link" aria-current="page" href="{{ route('admin.messagges.index') }}">
-                                <span><i class="fa-solid fa-envelope"></i> Messaggi</span></a>
-                        </li>
-                        <li class="nav-item dropstart text-md-center">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                <i class="fa-solid fa-user my_icon d-none d-md-inline-block d-lg-inline-block"></i>
-                                <span class="d-block fw-bold fs-5 d-md-none">{{ Auth::user()->email }}</span>
-                                <span
-                                    class="d-none d-md-block mt-1">{{ Auth::user()->first_name . ' ' . Auth::user()->last_name }}</span>
-                            </a>
-                            <ul class="dropdown-menu ">
-                                <li> <a class="dropdown-item"
-                                        href="{{ route('admin.index') }}">{{ __('Dashboard') }}</a></li>
-                                <li> <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                        class="d-none">
-                                        @csrf
-                                    </form>
+                        <!-- Elementi di navigazione visibili solo agli ospiti (non autenticati) -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Accedi</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Registrati</a>
                                 </li>
-                            </ul>
-                        </li>
+                            @endif
+                        @else
+                            <!-- Elementi di navigazione visibili solo agli utenti autenticati -->
+                            <li class="nav-item d-inline-block d-md-none d-lg-none">
+                                <a id="logo_debug" href="http://localhost:5174/"
+                                    class="align-items-center mb-3 mb-md-0 me-md-auto d-flex w-25">
+                                    <img class="img-fluid" src="{{ asset('img/small-logo.png') }}" alt="logo">
+                                </a>
+                            </li>
+                            <li class="nav-item d-inline-block d-md-none d-lg-none">
+                                <a class="nav-link" aria-current="page" href="{{ route('admin.apartments.index') }}">
+                                    <span><i class="fa-solid fa-list"></i> Appartamenti</span>
+                                </a>
+                            </li>
+                            <li class="nav-item d-inline-block d-md-none d-lg-none">
+                                <a class="nav-link" aria-current="page" href="{{ route('admin.apartments.create') }}">
+                                    <span><i class="fa-solid fa-plus"></i> Nuovo Appartamento</span>
+                                </a>
+                            </li>
+                            <li class="nav-item d-inline-block d-md-none d-lg-none">
+                                <a class="nav-link" aria-current="page" href="{{ route('admin.sponsorships.payment') }}">
+                                    <span><i class="fa-solid fa-star"></i> Sponsorizza appartamento</span>
+                                </a>
+                            </li>
+                            <li class="nav-item d-inline-block d-md-none d-lg-none">
+                                <a class="nav-link" aria-current="page" href="{{ route('admin.messagges.index') }}">
+                                    <span><i class="fa-solid fa-envelope"></i> Messaggi</span>
+                                </a>
+                            </li>
+                            <li class="nav-item dropstart text-md-center">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <i class="fa-solid fa-user my_icon d-none d-md-inline-block d-lg-inline-block"></i>
+                                    <span class="d-block fw-bold fs-5 d-md-none">{{ Auth::user()->email }}</span>
+                                    <span
+                                        class="d-none d-md-block mt-1">{{ Auth::user()->first_name . ' ' . Auth::user()->last_name }}</span>
+                                </a>
+                                <ul class="dropdown-menu ">
+                                    <li>
+                                        <a class="dropdown-item"
+                                            href="{{ route('admin.index') }}">{{ __('Dashboard') }}</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
                     </ul>
                 </div>
             </div>
         </div>
     </nav>
+
 </header>
